fd1aef913ade52672a7d39966533a43e823002d7
Asanka Jayasundara <asanka.jsndr@gmail.com>
2019-10-09 12:43:45 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,10 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgmHzAbTzKE6OCCOlFRc9DF_kactfXNCQ",
-  authDomain: "fitness-app-98135.firebaseapp.com",
-  databaseURL: "https://fitness-app-98135.firebaseio.com",
-  projectId: "fitness-app-98135",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
 };
 
